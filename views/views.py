b3c214fd8f63e32ff1219dd8b16da96a2d2c9698
hakishyntassov <khaknazarshyntassov@gmail.com>
2026-01-04 22:11:02 -0500

    edited join button

--- views/views.py
+++ views/views.py
@@ -2,10 +2,10 @@ import discord
 from discord.ext import commands
 
 class JoinButton(discord.ui.View):
-    def __init__(self, title: str):
+    def __init__(self, title):
         super().__init__(timeout=180) # Optional: set a timeout for the view
         self.title = title
 
     @discord.ui.button(label="Join", style=discord.ButtonStyle.success)
-    async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button, title: str):
-        await interaction.response.send_message(f"You joined the {title} event!", ephemeral=True)
+    async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message(f"You joined the {self.title} event!", ephemeral=True)
