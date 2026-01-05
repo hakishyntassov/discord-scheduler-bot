64d06c4d641944fe0dfe728064311d2937c2339f
hakishyntassov <khaknazarshyntassov@gmail.com>
2026-01-05 13:46:44 -0500

    fixed the order

--- views/views.py
+++ views/views.py
@@ -10,42 +10,45 @@ class JoinButton(discord.ui.View):
 
     @discord.ui.button(label="Join", style=discord.ButtonStyle.success, row=0)
     async def join_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.defer()  # no visible reply
+        await interaction.response.defer(ephemeral=True)  # no visible reply
+
         user = interaction.user
-        # prevent double-join
-        if user.id in joined_users:
-            await interaction.response.send_message(
-                "You already joined.",
-                ephemeral=True
-            )
-            return
-
-        joined_users.add(interaction.user.id)
-
-        # rebuild embed
-        embed = interaction.message.embeds[0]
-        embed.set_field_at(
-            0,
-            name="👥 Joined",
-            value=str(len(joined_users)),
-            inline=False
-        )
-        await interaction.message.edit(embed=embed, view=self)
 
         try:
-            await interaction.response.send_message(
-                f"📩 You joined the **{self.title}** event!\n📩 I’ve sent you a DM!",
-                ephemeral=True
+            # prevent double-join
+            if user.id in joined_users:
+                await interaction.response.send_message(
+                    "You already joined.",
+                    ephemeral=True
+                )
+                return
+
+            joined_users.add(interaction.user.id)
+
+            # rebuild embed
+            embed = interaction.message.embeds[0]
+            embed.set_field_at(
+                0,
+                name="👥 Joined",
+                value=str(len(joined_users)),
+                inline=False
             )
+            await interaction.message.edit(embed=embed, view=self)
+
             dm = await user.create_dm()
             msg = await dm.send(
                 f"👋 Hi! You joined **{self.title}** event.\n📩 Here’s your private scheduler form."
+            )
+            # optional confirmation (ephemeral)
+            await interaction.followup.send(
+                f"📩 I’ve sent you a DM for **{self.title}**.",
+                ephemeral=True
             )
             await asyncio.sleep(1800)
             await msg.delete()
         except discord.Forbidden:
             # User has DMs closed
-            await interaction.response.send_message(
+            await interaction.followup.send(
                 "❌ I can’t DM you. Please enable DMs from server members.",
                 ephemeral=True
             )
