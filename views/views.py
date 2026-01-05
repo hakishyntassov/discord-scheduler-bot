a327b87cd85c8a4c0b6b2d59a440e3728dc150a7
hakishyntassov <khaknazarshyntassov@gmail.com>
2026-01-04 22:46:40 -0500

    added dm function

--- views/views.py
+++ views/views.py
@@ -8,4 +8,19 @@ class JoinButton(discord.ui.View):
 
     @discord.ui.button(label="Join", style=discord.ButtonStyle.success)
     async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message(f"You joined the **{self.title}** event!", ephemeral=True)
+        user = interaction.user
+        try:
+            dm = await user.create_dm()
+            await dm.send(
+                f"👋 Hi! You joined **{self.title}** event. Here’s your private scheduler form."
+            )
+            await interaction.response.send_message(
+                f"📩 You joined the **{self.title}** event!\n📩 I’ve sent you a DM!",
+                ephemeral=True
+            )
+        except discord.Forbidden:
+            # User has DMs closed
+            await interaction.response.send_message(
+                "❌ I can’t DM you. Please enable DMs from server members.",
+                ephemeral=True
+            )
