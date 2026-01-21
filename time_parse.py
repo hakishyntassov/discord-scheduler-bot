9c6e121027d73b5cedda83e9de6a0c380970adf6
hakishyntassov <khaknazarshyntassov@gmail.com>
2026-01-21 18:46:19 -0500

    added midnight identifier

--- time_parse.py
+++ time_parse.py
@@ -1,4 +1,5 @@
 # Helper module to process time
+from datetime import datetime
 
 def to_minutes(hour, minute=0, ampm=None):
     hour = int(hour)
@@ -20,10 +21,23 @@ def to_minutes(hour, minute=0, ampm=None):
     return total
 
 def minutes_to_label(minutes: int) -> str:
+    if int(minutes) == 1439:
+        return "midnight"
+    else:
+        minutes = int(minutes)
+        hour = int(minutes // 60)
+        minute = int(minutes % 60)
+        ampm = "am" if hour < 12 else "pm"
+        hour = hour % 12
+        hour = 12 if hour == 0 else hour
+        return f"{hour}:{minute:02d}{ampm}"
+
+def time_to_label(weekday: int, minutes: int) -> datetime:
     minutes = int(minutes)
     hour = int(minutes // 60)
     minute = int(minutes % 60)
-    ampm = "am" if hour < 12 else "pm"
-    hour = hour % 12
-    hour = 12 if hour == 0 else hour
-    return f"{hour}:{minute:02d}{ampm}"
+    year = 2026
+    month = 1
+    day = 22
+    dt = datetime(year, month, day, hour, minute)
+    return dt
