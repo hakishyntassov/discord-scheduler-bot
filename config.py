a6fbe164938cb039d7a9a9f8a0f41c151549e013
hakishyntassov <khaknazarshyntassov@gmail.com>
2026-01-31 10:49:48 -0500

    delete mysql vars

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 import os
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-MYSQL_PUBLIC_URL = os.getenv("MYSQL_PUBLIC_URL")
-MYSQL_HOST = os.getenv("MYSQLHOST")
-MYSQL_PORT = int(os.getenv("MYSQLPORT"))
-MYSQL_USER = os.getenv("MYSQLUSER")
-MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
 DATABASE_PUBLIC_URL = os.getenv("DATABASE_PUBLIC_URL")
 
 DAY_NAMES = [
